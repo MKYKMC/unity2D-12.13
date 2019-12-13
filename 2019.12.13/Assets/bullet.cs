06bdc0cc32c2a62fd8035f7fc6f0d1a3b9f105ba
MKYKMC <Lisa88052468@gmail.com>
2019-12-13 15:02:12 +0800

    子彈

---
+++ 2019.12.13/Assets/bullet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.Translate( 0.5f, 0, 0 );
+		
+	}
+}
--- 2019.12.13/Assets/player.cs
+++ 2019.12.13/Assets/player.cs
@@ -1,17 +1,64 @@
 ﻿using UnityEngine;
 
 public class player : MonoBehaviour {
+
+    public float speedbullet=1000;
+
     #region 欄位
 	
-	[Header("移動速度"),Range(0,2)]
-	public float speed=1.5f;
+	[Header("移動速度"),Range(-20,20)]
+	public float speed=9.4f;
 	
 	[Header("血量"),Range(0,100)]
 	public float blood=100f;
+    
+	[Header("子彈")]
+	public GameObject bullet;
+	[Header("音效")]
+	public AudioClip source;
+    [Header("發射位置")]
+	public Transform point;
 
-	public Animator ani;
-	public Rigidbody2D r2d;
+	private Animator ani;
+	private Rigidbody2D r2d;
+	private AudioSource boo;
     
 	#endregion
 	
+ private void Start(){
+	  
+	  ani=GetComponent<Animator>();
+      r2d=GetComponent<Rigidbody2D>(); 
+	  boo=GetComponent<AudioSource>();   
+
+ }
+private void FixedUpdate(){
+
+      Move();
+	  shoot();
+
 	}
+	
+private void Move(){
+
+  float S=Input.GetAxisRaw("Horizontal");
+	r2d.AddForce(new Vector2(speed*S,0));
+	if(S!=0){
+		transform.localScale=new Vector3(S, 1, 1);
+	}
+	
+
+	}
+
+private void shoot(){
+	if( Input.GetMouseButtonDown(0) ){
+		boo.PlayOneShot(source);
+		GameObject temp=Instantiate( bullet,point.position,Quaternion.identity );
+		
+		temp.GetComponent<Rigidbody2D> ().AddForce ( new Vector2(speedbullet*transform.localScale.x,80));　
+
+	}
+
+}
+
+}

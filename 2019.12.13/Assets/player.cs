a133484f9d6fc372a75c52653f15db8ecf2b0f90
MKYKMC <Lisa88052468@gmail.com>
2019-12-13 12:12:14 +0800

    場景、人物

---
+++ 2019.12.13/Assets/player.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class player : MonoBehaviour {
+    #region 欄位
+	
+	[Header("移動速度"),Range(0,2)]
+	public float speed=1.5f;
+	
+	[Header("血量"),Range(0,100)]
+	public float blood=100f;
+
+	public Animator ani;
+	public Rigidbody2D r2d;
+    
+	#endregion
+	
+	}
